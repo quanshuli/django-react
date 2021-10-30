0e5c6ec23583657b85392b688ba09d3fd98c0dd2
quanshuli <quanshuli.dev@gmail.com>
2021-10-30 11:59:41 +1000

    npm i react

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -7,9 +7,9 @@ export default class App extends Component{
     }
 
     render(){
-        return (<h1>Testing React Code</h1>)
+        return (<h1>{ this.props.name }</h1>);
     }
 }
 
 const appDiv = document.getElementById('app');
-render(<App />, appDiv);
+render(<App name='hello'/>, appDiv);
