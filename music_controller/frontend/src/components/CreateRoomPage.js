398c8731472f1218ccf56297a31c7f973af8805c
quanshuli <quanshuli.dev@gmail.com>
2021-10-30 18:15:12 +1000

    Update CreateRoomPage.js

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Button } from '@material-ui/core/Button'
+import { Grid } from '@material-ui/core'
+import { Typography } from '@material-ui/core';
+import { FormHelperText } from '@material-ui/core/FormHelperText';
+import { FormControl } from '@material-ui/core/FormControl';
+import { Link } from 'react-router-dom/Link';
+import { Radio } from '@material-ui/core/Radio';
+import { RadioGroup } from '@material-ui/core/RadioGroup';
+import { FormControlLabel } from '@material-ui/core/FormControlLabel';
 
 export default class CreateRoomPage extends Component{
     constructor(props){
@@ -7,6 +16,12 @@ export default class CreateRoomPage extends Component{
     }
 
     render(){
-        return (<h1>create room page</h1>);
+        return (<Grid container spaceing={1}>
+            <Grid item xs={12} align='center'>
+                <Typography component='h4' variant='h4'>
+                    Create A Room
+                </Typography>
+            </Grid>
+        </Grid>);
     }
 }
