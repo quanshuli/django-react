19d8a20f63657ceec0655d918857ffa7b114e6d7
quanshuli <quanshuli.dev@gmail.com>
2021-10-30 19:34:15 +1000

    Update CreateRoomPage.js

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core'
 import { Grid, TextField } from '@material-ui/core'
 import { Typography } from '@material-ui/core';
 import { FormHelperText } from '@material-ui/core';
 import { FormControl } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import { Radio } from '@material-ui/core';
 import { RadioGroup } from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
@@ -13,6 +13,40 @@ import { FormControlLabel } from '@material-ui/core';
 export default class CreateRoomPage extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        }
+
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+        this.handelGuestCanPauseChange = this.handelGuestCanPauseChange(this);
+        this.handelRoomButtonPressed = this.handelRoomButtonPressed.bind(this);
+    }
+
+    handleVotesChange(e){
+        this.setState({
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handelGuestCanPauseChange(e){
+        this.setState({
+            guestCanPause: e.target.value === 'true' ? true : false,
+        });
+    }
+
+    handelRoomButtonPressed(){
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              votes_to_skip: this.state.votesToSkip,
+              guest_can_pause: this.state.guestCanPause,
+            }),
+          };
+        fetch("/api/create-room", requestOptions)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }
 
     render(){
@@ -29,7 +63,8 @@ export default class CreateRoomPage extends Component{
                             Guest Control of Playback State
                         </div>
                     </FormHelperText>
-                    <RadioGroup row defaultValue='true'>
+                    <RadioGroup row defaultValue='true'
+                                    onChange={this.handelGuestCanPauseChange}>
                         <FormControlLabel value='true' 
                             control={<Radio color='primary' />}
                             label='Play/Pause'
@@ -46,6 +81,7 @@ export default class CreateRoomPage extends Component{
             <Grid item xs={12} align='center'>
                 <FormControl>
                     <TextField required={true} 
+                               onChange={this.handleVotesChange}
                                type='number'
                                defaultValue={this.defaultVotes}
                                inputProps={{
@@ -59,8 +95,9 @@ export default class CreateRoomPage extends Component{
                 </FormControl>
             </Grid>
             <Grid item xs={12} align='center'>
-                <Button color='secondary' variant='contained' to='/'
-                        component={Link}>
+                <Button color='secondary' variant='contained'
+                        onClick={this.handelRoomButtonPressed}
+                >
                     Create A Room
                 </Button>
             </Grid>
