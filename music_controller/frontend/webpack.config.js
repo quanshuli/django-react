de128be79cd3ef42609434a19bc27c7b05b3db79
quanshuli <quanshuli.dev@gmail.com>
2021-10-30 11:59:44 +1000

    Update webpack.config.js

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -1,31 +1,35 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "./static/frontend"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
+module.exports = (env, argv) => {
+    const mode = argv.mode || 'development'; // dev mode by default
+
+    return {
+        entry: "./src/index.js",
+        output: {
+            path: path.resolve(__dirname, "./static/frontend"),
+            filename: "[name].js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
                     loader: "babel-loader",
-                }
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production"),
-            }
-        })
-    ]
-}
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+            ],
+        },
+        optimization: {
+            minimize: true,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(mode)
+            }),
+        ],
+    };
+
+};
