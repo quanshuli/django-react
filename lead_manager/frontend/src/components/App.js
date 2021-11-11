c887bcc06f588eacf3e44eb8cc915107d87620dc
quanshuli <quanshuli.dev@gmail.com>
2021-11-11 15:39:21 +1000

    lesson3 15:39

--- lead_manager/frontend/src/components/App.js
+++ lead_manager/frontend/src/components/App.js
@@ -4,12 +4,17 @@ import { ReactDOM, render } from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 
+import { Provider } from "react-redux";
+import store from "../store";
+
 const App = () => {
   return (
-    <Fragment>
-      <Header />
-      <Dashboard />
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Header />
+        <Dashboard />
+      </Fragment>
+    </Provider>
   );
 };
 
