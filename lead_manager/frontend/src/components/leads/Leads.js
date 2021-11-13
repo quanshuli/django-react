b2ac842138fccd231a9dc92eeb14efaef4186817
quanshuli <quanshuli.dev@gmail.com>
2021-11-13 14:04:03 +1000

    create

--- lead_manager/frontend/src/components/leads/Leads.js
+++ lead_manager/frontend/src/components/leads/Leads.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
 const Leads = () => {
   return (
@@ -8,4 +11,8 @@ const Leads = () => {
   );
 };
 
-export default Leads;
+const mapStateToPress = (state) => ({
+  leads: state.leads.leads,
+});
+
+export default connect(Leads);
